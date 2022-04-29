d4ce6b48902721728dc37a1f632f60cee275feda
fedem <fedemv93@gmail.com>
2022-04-29 19:57:02 -0300

    final de leccion 4

---
+++ Leccion04/04-01-5-SentenciasControl.js
@@ -0,0 +1,23 @@
+let mes = 11;
+let estacion = "estacion desconocida";
+
+switch ( mes ){
+    case 1: case 2: case 12:
+        estacion = "invierno";
+        break;
+    case 3: case 4: case 5:
+        estacion = "primavera";
+        break;
+    case 6: case 7: case 8:
+        estacion = "invierno";
+        break;
+    case 9: case 10: case 11:
+        estacion = "otoño";
+        break;
+    default:
+        estacion = "valor incorrecto";
+}
+
+console.log(estacion);
+
+//swirch no sirve si el numero es cadena, switch compara lo mismo tipos estrictamente  
