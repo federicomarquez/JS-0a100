b86d38ca08e9024c723303daaea53c326aa435c4
fedem <fedemv93@gmail.com>
2022-04-29 19:39:39 -0300

    agregados leccion 4

---
+++ Leccion04/04-01-4-SentenciasControl.js
@@ -0,0 +1,26 @@
+let numero = 9;
+
+let numeroTexto = "valor desconocido";
+
+switch(numero){
+    case 1:
+        numeroTexto = "numero uno";
+        break;
+    case 2:
+        numeroTexto = "numero dos";
+        break;
+    case 3:
+        numeroTexto = "numero 3";
+        break;
+    case 4:
+        numeroTexto = "numero 4";
+        break;
+    case 5:
+        numeroTexto = "numero 5";
+        break;
+    default:
+        numeroTexto = "caso no encontrado";
+     
+}
+
+console.log(numeroTexto);
