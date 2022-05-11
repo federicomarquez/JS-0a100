8675f973a030482b21ba100f8868ffd272998bbb
fedem <fedemv93@gmail.com>
2022-05-10 21:30:50 -0300

    agregado de leccion 8

---
+++ Leccion08/08-01-03-Objetos.js
@@ -0,0 +1,21 @@
+
+let persona= {
+    nombre: "Juan",
+    apellido: "Perez",
+    email:"jperez@mail.com",
+    edad: 28,
+    nombreCompleto: function(){
+        return this.nombre + " " + this.apellido;
+    }
+}
+
+
+console.log(persona.nombre);
+console.log(persona["apellido"]);
+
+// for in
+
+for(nombrePropiedad in persona){
+    console.log(propiedad);
+    console.log(persona[nombrePropiedad]);
+}
---
+++ Leccion08/08-01-04-Objetos.js
@@ -0,0 +1,24 @@
+let persona= {
+    nombre: "Juan",
+    apellido: "Perez",
+    email:"jperez@mail.com",
+    edad: 28,
+    nombreCompleto: function(){
+        return this.nombre + " " + this.apellido;
+    }
+}
+
+persona.tel = "54632153";
+persona.tel = ""
+
+console.log(persona);
+dffggfdfgddfgs
+
+DocumentFragment
+DocumentFragmentgf
+gf
+
+gffg
+fg
+
+f
