fc3559a01f93b9f7219bb3054eb8ad0b27612e6a
federicomarquez <80365596+federicomarquez@users.noreply.github.com>
2022-06-29 22:54:55 -0300

    retormando

--- Leccion09/09-01-03-Clases.js
+++ Leccion09/09-01-03-Clases.js
@@ -1,3 +1,7 @@
+//No es posible crear objetos antes de declarar la clase
+//No se aplica el concepto de hoisting 
+//let persona2 = new Persona("karla", "Juarez");
+
 class Persona {
     constructor(nombre, apellido){
         this._nombre = nombre;
@@ -16,3 +20,7 @@ class Persona {
 let persona1 = new Persona("Juan","Perez");
 persona1.nombre = "juan Carlos"; //set nombre
 console.log(persona1.nombre);//get nombre
+
+
+let persona2 = new Persona("karla", "Juarez");
+console.log(persona2);
